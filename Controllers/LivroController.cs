4386c78f38c102dabd349cb91e5066baf2a5fb20
Herik Spada <gryzhn1@gmail.com>
2024-12-03 11:51:16 -0300

    Começando o método de cadastrar

--- Controllers/LivroController.cs
+++ Controllers/LivroController.cs
@@ -1,3 +1,4 @@
+using System.Net.Mime;
 using Bibliotec.Contexts;
 using Bibliotec.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -30,6 +31,38 @@ namespace Bibliotec_MVC.Controllers
             ViewBag.LivrosComReserva = livrosReservados;
             
             return View();
+        }
+
+
+
+        // Método que retorna a tela de cadastro: 
+    	[Route("Cadastro")]
+        public IActionResult Cadastro()
+        {
+            ViewBag.Admin = HttpContext.Session.GetString("Admin")!;
+            ViewBag.Categorias = context.Categoria.ToList();
+            // Retorna a View de cadastro:
+            return View();
+        }
+
+
+        // Método para cadastrar um livro:
+        [Route("Cadastrar")]
+        public IActionResult Cadastrar(IFormCollection form)
+        {
+            Livro novoLivro = new Livro();
+
+            // O que meu usuário escrever no formulário será atribuido ao novoLivro
+
+            novoLivro.Nome = form ["Nome"].ToString();
+            novoLivro.Descricao = form ["Descricao"].ToString();
+            novoLivro.Escritor = form ["Escritor"].ToString();
+            novoLivro.Editora = form ["Editora"].ToString();
+            novoLivro.Idioma = form ["Idioma"].ToString();
+
+            context.Livro.Add(novoLivro);
+            context.SaveChanges();
+
         }
 
         // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
